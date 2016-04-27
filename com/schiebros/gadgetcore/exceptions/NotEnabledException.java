968abacb0b6e564d117d6a84a7928de0f316785f
LakkieGaming <lakkiegaming@gmail.com>
2016-04-26 20:51:26 -0400

    Create NotEnabledException.java

---
+++ com/schiebros/gadgetcore/exceptions/NotEnabledException.java
@@ -0,0 +1,7 @@
+package com.schiebros.gadgetcore.exceptions;
+
+public class NotEnabledException extends Exception {
+	private static final long serialVersionUID = 1422453183351443574L;
+	public NotEnabledException() {}
+	public NotEnabledException(String msg) {super(msg);}
+}
