ba0d06b9f12c4a02c8f446374e30805685c5db3e
LakkieGaming <lakkiegaming@gmail.com>
2016-04-26 20:52:24 -0400

    Create VariableNotChangableException.java
    I technically could have just simply made the variable final, but the constructor of some classes request that a variable must be defined in the constructor if it is final.

---
+++ com/schiebros/gadgetcore/exceptions/VariableNotChangableException.java
@@ -0,0 +1,7 @@
+package com.schiebros.gadgetcore.exceptions;
+
+public class VariableNotChangableException extends Exception {
+	private static final long serialVersionUID = 4100904415835223554L;
+	public VariableNotChangableException() {}
+	public VariableNotChangableException(String msg) {super(msg);}
+}
