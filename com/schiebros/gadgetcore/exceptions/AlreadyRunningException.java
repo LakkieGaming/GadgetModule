fc9306afc2bd88aab1a0c1bbcd2dbb612bae913d
LakkieGaming <lakkiegaming@gmail.com>
2016-04-26 20:51:14 -0400

    Create AlreadyRunningException.java

---
+++ com/schiebros/gadgetcore/exceptions/AlreadyRunningException.java
@@ -0,0 +1,7 @@
+package com.schiebros.gadgetcore.exceptions;
+
+public class AlreadyRunningException extends Exception {
+	private static final long serialVersionUID = 1186107608307847998L;
+	public AlreadyRunningException() {}
+	public AlreadyRunningException(String msg) {super(msg);}
+}
